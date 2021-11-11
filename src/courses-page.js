4416db0167289517e909e5c1fa71ab6ea022c802
Tin Nguyen <hopthucuatin@gmail.com>
2021-11-11 11:55:07 +0700

    Bar divide for .weeks

--- src/courses-page.js
+++ src/courses-page.js
@@ -1,20 +1,26 @@
 const TOPIC_HEIGHT = "500px";
 // create a css class and add it to the html
 
-$(document).ready(function () {
+function setScroll(selector) {
 	// if the screen is less than or equal to sm screen, then display grid template columns of repeat(1, 1fr)
-	if ($(window).width() <= 768) {
-		$(".topics").css({ display: "grid", gridTemplateColumns: "repeat(1, 1fr)" });
+	if ($(window).width() <= 768) { $(selector).css({ display: "grid", gridTemplateColumns: "repeat(1, 1fr)" });
 	}
 	// if the screen is less than or equal to md screen, then display grid template columns of repeat(2, 1fr)
 	else if ($(window).width() <= 992) {
-		$(".topics").css({ display: "grid", gridTemplateColumns: "repeat(2, 1fr)" });
+		$(selector).css({ display: "grid", gridTemplateColumns: "repeat(2, 1fr)" });
 	}
 	else {
-		$(".topics").css({ display: "grid", gridTemplateColumns: "repeat(4, 1fr)" });
+		$(selector).css({ display: "grid", gridTemplateColumns: "repeat(4, 1fr)" });
 	}
+}
+
+
+$(document).ready(function () {
 
 	$("ul.section").css("overflow-y", "scroll").css("height", TOPIC_HEIGHT);
+
+	setScroll('.topics');
+	setScroll('.weeks');
 
 	$(".has-blocks.mb-3").removeClass("has-blocks").css("width", "full");
 
---
+++ src/home-page.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	var homePage = $('#frontpage-course-list > div');
+
+	console.log('homePage', homePage)
+
+})
