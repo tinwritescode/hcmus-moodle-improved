762507152046405a784fee4711ccf01e83d29ef2
Tin-Trung Nguyen <hopthucuatin@gmail.com>
2022-01-14 15:42:39 +0700

    Moodle events

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,9 @@
 const username = $("#username");
 const password = $("#password");
 const autoLogin = $("#autoLogin");
+const token = $("#token");
+const loginButton = $(`[role="loginButton"]`);
+const editableLog = $(`[role="editableLog"]`);
 
 const STORAGE_SELECTOR = "[id]";
 let debounceTimer;
@@ -41,4 +44,27 @@ function collectData() {
   for (const el of document.querySelectorAll(STORAGE_SELECTOR))
     data[el.id] = el.type === "checkbox" ? el.checked : el.value;
   return data;
+}
+
+//
+loginButton.click(onLoginButtonClick);
+
+async function onLoginButtonClick() {
+  // get token
+  const res = await fetch(`https://courses.ctda.hcmus.edu.vn/login/token.php?username=${username.val()}&password=${password.val()}&service=moodle_mobile_app`)
+  const data = await res.json()
+
+  token.val(data.token)
+  //append log to editable log
+  appendToEditableLog(`Get token thành công: ${data.token}, token đã được lưu`)
+  //tính năng lấy event ở trang fb đã hoạt động
+  appendToEditableLog(`<strong>Tính năng lấy event ở trang fb đã hoạt động</strong>`)
+
+  // alert(JSON.stringify(data))
+  doSave();
+}
+
+//append to editableLog
+function appendToEditableLog(text) {
+  editableLog.append(`${new Date().toLocaleString()} - ${text} <br>`)
 }
